cf007d12a68704327f60d35920a3f46853c7aaf9
mahditaghinezhad1404-maker <mahdi.taghinezhad1404@gmail.com>
2026-07-05 13:14:49 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-     from fastapi import FastAPI
-     app = FastAPI()
+from fastapi import FastAPI
+app = FastAPI()
 
-     @app.get("/")
-     def read_root():
-         return {"message": "MR Manage is live!"}
+@app.get("/")
+def read_root():
+    return {"message": "MR Manage is active"}
+     
